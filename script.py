e3682f975ab690c1ae478eb77dadd0af0ed58a2a
Paweł Cegła <3962186+pawelcegla@users.noreply.github.com>
2023-12-13 12:28:06 +0100

    Initial commit

---
+++ script.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+import tomllib
+
+import requests
+
+logging.basicConfig(filename='/var/log/dyndns.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+logging.debug('Requests version: %s', requests.__version__)
+ip = requests.get('https://api.ipify.org').text
+logging.debug('Public IP: %s', ip)
+with open(sys.argv[1], 'rb') as f:
+    cfg = tomllib.load(f)
+    for hostname in cfg['credentials'].keys():
+        logging.debug('Hostname: %s', hostname)
+        creds = cfg['credentials'][hostname]
+        dyndns_url = 'https://www.ovh.com/nic/update?system=dyndns&hostname={}&myip={}'.format(hostname, ip)
+        dyndns = requests.get(dyndns_url, auth=(creds['username'], creds['password'])).text
+        logging.debug('DynDNS responded: %s', dyndns)
